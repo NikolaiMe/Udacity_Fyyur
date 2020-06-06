fef89b7b7832f737f93be79e332de532222ee239
Nikolai <nikolai.merz@gmail.com>
2020-06-06 20:30:56 +0200

    Ferdisch

--- projects/03_coffee_shop_full_stack/coffee_shop_NikolaiMerz/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/coffee_shop_NikolaiMerz/frontend/src/environments/environment.ts
@@ -2,13 +2,17 @@
  * ensure all variables on this page match your project
  */
 
+// https://fsnd-nikolai.eu.auth0.com/authorize?audience=coffeeshop&response_type=token&client_id=TqIHsl2HnAEuuZxv9HFXghjP4Hfj5D2F&redirect_uri=http://localhost:8080/login-results
+
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'fsnd-nikolai.eu', // the auth0 domain prefix
+    audience: 'coffeeshop', // the audience set for the auth0 app
+    clientId: 'TqIHsl2HnAEuuZxv9HFXghjP4Hfj5D2F', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
+
+
